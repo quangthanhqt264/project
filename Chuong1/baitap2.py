ccfba6136a585d49fba45d151d79acab5daa1b39
quangthanhqt264 <thanhqt064@gmail.com>
2023-03-10 15:07:29 +0700

    hi

---
+++ Chuong1/baitap2.py
@@ -0,0 +1,8 @@
+Myage=5
+print('you will be ' +str(int(Myage) + 1) + ' in a year.' )
+print('you will be ' +str(int('4') + 1) + ' in a year.' )
+print('you will be ' +str( 4+1)  + ' in a year.' )
+print('you will be ' +str( 5 )  + ' in a year.' )
+print('you will be ' + '5' + ' in a year.' )
+print('you will be 5 ' + ' in a year.' )
+print('you will be 5 in a year.')
