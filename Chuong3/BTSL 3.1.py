d85f7f1a5d08e7d0039e144833d5f47648d555e4
quangthanhqt264 <thanhqt064@gmail.com>
2023-03-17 14:13:12 +0700

    chao

---
+++ Chuong3/BTSL 3.1.py
@@ -0,0 +1,17 @@
+A=float(input("a=" ))
+B=float(input("b=" ))
+C=float(input("c=" ))
+max=A
+if max<B:
+    max=B
+elif max<C:
+    max=C
+print("SLN=",max)
+min=A
+if min>B:
+    min=B
+elif min>C:
+    min=C
+print("SBN=",min)
+    
+
---
+++ Chuong3/BTSL 3.2.py
@@ -0,0 +1,5 @@
+MAY=(int(input("So may=" )))
+if MAY>=5:
+    print("So tien=",MAY*450)
+else:
+    print("So tien=",MAY*500)
